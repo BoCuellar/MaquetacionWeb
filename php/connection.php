9c1475153b975a39c4e8b26a5d3cfd75b908b49a
Bo Cuéllar Cózar <82579120+BoCuellar@users.noreply.github.com>
2022-03-08 16:22:40 +0100

    conexión bdd

---
+++ php/connection.php
@@ -0,0 +1,20 @@
+<?php
+    // Class que conecta con la base de datos
+    class SQLConnection extends PDO{
+        private $host = "localhost"; // host para el localhost
+        private $dbname = "datosUsuario"; // Nombre de BDD
+        private $user = "root"; // Nombre de usuario de BDD
+        private $pass = ""; // Contraseña de BDD
+
+        public function __construct()
+        {
+            try{
+                // Conectar a la BDD mediante PDO
+                parent::__construct("mysql:host=$this->host;dbname=$this->dbname;charset=utf8", $this->user, $this->pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            }catch(PDOException $e){
+                echo "Error: " . $e->getMessage();
+                exit;
+            }
+        }
+    }
+?>
